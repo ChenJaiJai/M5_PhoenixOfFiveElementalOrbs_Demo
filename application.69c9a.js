820703ec16c6e1d91e3b64bfd08b04fd0878ee4f
JaiJai <wm.rd01@gmail.com>
2025-04-28 10:36:54 +0800

    更新底層

--- application.69c9a.js
+++ application.69c9a.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.70372.json';
+          this.settingsPath = 'src/settings.58640.json';
           this.showFPS = false;
         }
         _createClass(Application, [{

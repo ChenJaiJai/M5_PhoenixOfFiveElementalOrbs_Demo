c15b850f0046ddf7b39ae4683398a723ae3c6109
JaiJai <wm.rd01@gmail.com>
2025-05-14 09:29:07 +0800

    大更新

--- index.6f7a5.js
+++ index.6f7a5.js
@@ -1,4 +1,4 @@
-System.register(["./application.69c9a.js"], function (_export, _context) {
+System.register(["./application.087de.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
